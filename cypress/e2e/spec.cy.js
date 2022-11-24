51e9b99d336c25c8915cf087ac3ed895b4f7d985
Muhammed Atyib Ali <aatyib@gmail.com>
2022-11-24 16:13:02 +0000

    ...

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -23,6 +23,7 @@ describe('testing question input box', () => {
 
   })
 })
+*/
 //Testing  the input of topic button  
 //does the submit button work 
 //.submit() doesn't work because we dont have a form 
@@ -30,30 +31,30 @@ describe('testing question input box', () => {
 describe('testing question input box', () => {
   it('Gets,types and asserts', () => {
     cy.visit('http://localhost:3000')
-    cy.get('textarea[name="Topic"').should('be.empty')
-    cy.get('textarea[name="Topic"]').type('HTML/CSS')
-    cy.get('button').click()
+    //cy.get('textarea[name="Topic"').should('be.empty')
+    cy.get('select[name="topic"]').select(1)
+    //cy.get('button').click()
     
   })
 })
-*/
+
 //edit button doesnt have a form or a class so  can't click it 
 
-describe('testing checkbox', () => {
-  it('does checkbox work', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('input[id="react"]').should('be.empty')
+//describe('testing checkbox', () => {
+  //it('does checkbox work', () => {
+    //cy.visit('http://localhost:3000')
+    //cy.get('input[id="react"]').should('be.empty')
    // cy.get('input[id="HTML_CSS"]').should('be.empty')
     //cy.get('input[id="SQL"]').should('be.empty')
    // cy.get('input[id="JS"]').should('be.empty')
-    cy.get('input[id="react"]').first().check()
+   // cy.get('input[id="react"]').first().check()
    // cy.get('input[id="HTML_CSS"]').first().check()
    // cy.get('input[id="SQL"]').first().check()
    // cy.get('input[id="JS"]').first().check()
-    cy.get('#crud-btn-container').click()
+    //cy.get('#crud-btn-container').click()
    // cy.get('button[class="hidden"]').click() doesnt have a class so can't work
-  })
-})
+  //})
+//})
 
 
 
